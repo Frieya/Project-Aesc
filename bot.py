4f91e36e99616c63ecff9c6e71d044a6ed278367
John Wesley Abe <68657618+Frieya@users.noreply.github.com>
2023-08-14 00:09:39 +0800

    added openai code

--- bot.py
+++ bot.py
@@ -2,17 +2,33 @@
 import os
 import discord
 from dotenv import load_dotenv
+import openai
+
 
 load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+# Set up the OpenAI API client
+openai.api_key = OPENAI_KEY
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
 @client.event
-async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+async def on_message(message):
+# Only respond to messages from other users, not from the bot itself
+  if message.author == client.user:
+    return
+  # Use the OpenAI API to generate a response to the message
+  response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt=f"{message.content}",
+  max_tokens=2048,
+  temperature=0.5,
+  )
+  # Send the response as a message
+  await message.channel.send(response.choices[0].text)
 
-# start the bot
-client.run(TOKEN)
+# Start the bot
+client.run("DISCORD_BOT_TOKEN")
