627a114d2bd5f4ccb21171aabb6ad90d2b4eedee
John Wesley Abe <68657618+Frieya@users.noreply.github.com>
2023-08-14 00:10:55 +0800

    changed name from bot.py to main.py

--- main.py
+++ main.py
@@ -15,18 +15,24 @@ openai.api_key = OPENAI_KEY
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 
+# trying to modify the code to respond only to the bot mention
 @client.event
 async def on_message(message):
-# Only respond to messages from other users, not from the bot itself
+  # Only respond to messages from other users, not from the bot itself
   if message.author == client.user:
     return
-  # Use the OpenAI API to generate a response to the message
-  response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=f"{message.content}",
-  max_tokens=2048,
-  temperature=0.5,
-  )
+  
+  # Check if the bot is mentioned in the message
+  if client.user in message.mentions:
+ 
+    # Use the OpenAI API to generate a response to the message
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=f"{message.content}",
+    max_tokens=2048,
+    temperature=0.5,
+    )
+
   # Send the response as a message
   await message.channel.send(response.choices[0].text)
 
